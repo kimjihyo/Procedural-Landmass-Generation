da70dcf782ddcf167135f82c6a718de995357b80
Jihyo Kim <kimjihyo0325@gmail.com>
2019-10-27 10:18:55 -0700

    initial commit

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor {
+    private static string GENERATE_BUTTON_TEXT = "Generate";
+    public override void OnInspectorGUI() {
+        MapGenerator mapGenerator = (MapGenerator)target;
+        
+        if (DrawDefaultInspector() && mapGenerator.autoUpdate) {
+            mapGenerator.GenerateMap();
+        }
+
+        if (GUILayout.Button(GENERATE_BUTTON_TEXT)) {
+            mapGenerator.GenerateMap();
+        }
+    }
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+    public bool autoUpdate;
+    public int octaves;
+    public int persistance;
+    public int lacunarity;
+    
+    public void GenerateMap() {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale,
+         octaves, persistance, lacunarity);
+        MapDisplay display = FindObjectOfType<MapDisplay> ();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
